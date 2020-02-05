e8ef3f1edde592cd81dfe4342c939b3d7e9a4676
vabatta <vabatta@gmail.com>
2020-02-05 11:54:23 +0100

    fix: 🐛 fixed asset manager picker and resize event

--- src/Plugin.vue
+++ src/Plugin.vue
@@ -14,18 +14,21 @@
 			v-bind:uid="uid"
 			field="src"
 			class="uk-button-small uk-margin-bottom"
-		/>
+		></sb-asset-selector>
 
-		<div class="uk-margin-bottom">
-			<img ref="imagePicker" v-bind:src="model.src" />
+		<div class="bordered uk-margin-bottom">
+			<div class="wrapper">
+				<img ref="imagePicker" class="uk-width-1-1" v-bind:src="model.src" />
+			</div>
 		</div>
+
 		<div>
 			Coordinates:
 			<input
 				class="uk-input uk-width-1-1"
 				type="text"
 				readonly
-				v-model="coordinates"
+				v-bind:value="coordinates"
 			/>
 		</div>
 	</div>
@@ -39,41 +42,52 @@ export default {
 	data() {
 		return {
 			picker: null,
-			coordinates: '{x: 0, y: 0}'
+			coordinates: ''
 		};
 	},
 	mounted() {
 		this.setPicker();
+		window.addEventListener('resize', this.handleResize);
+	},
+	beforeDestroy() {
+		window.removeEventListener('resize', this.handleResize);
 	},
 	methods: {
+		handleResize() {
+			this.updatePickerFocus({
+				x: parseFloat(this.model.x || this.getDefaults().x),
+				y: parseFloat(this.model.y || this.getDefaults().y)
+			});
+		},
 		setPicker() {
 			const { imagePicker } = this.$refs;
 
 			this.picker = new FocusPicker(imagePicker, {
-				onChange: focus => this.updateModelFocus(focus)
-				// focus: {
-				// 	x: parseFloat(this.model.x || this.getDefaults().x),
-				// 	y: parseFloat(this.model.y || this.getDefaults().y)
-				// }
+				onChange: focus => this.updateModelFocus(focus),
+				focus: {
+					x: parseFloat(this.model.x || this.getDefaults().x),
+					y: parseFloat(this.model.y || this.getDefaults().y)
+				}
 			});
 		},
 		updateModelFocus(focus) {
-			this.model.x = focus.x.toFixed(2);
-			this.model.y = focus.y.toFixed(2);
+			// console.log('update model focus', focus.x, focus.y);
+			this.model.x = parseFloat(focus.x).toFixed(2);
+			this.model.y = parseFloat(focus.y).toFixed(2);
 			this.coordinates = `{x: ${this.model.x}, y: ${this.model.y}}`;
 		},
 		updatePickerFocus(focus) {
 			if (this.picker) {
+				// console.log('update picker focus', focus.x, focus.y);
 				this.picker.setFocus({
-					x: focus.x.toFixed(2),
-					y: focus.x.toFixed(2)
+					x: parseFloat(focus.x).toFixed(2),
+					y: parseFloat(focus.y).toFixed(2)
 				});
 			}
 		},
 		getDefaults() {
 			return {
-				plugin: 'sbe-image-focus',
-				// src: 'https://picsum.photos/2400/1400',
+				plugin: 'ef-sbe-image-focus',
 				src: '',
 				x: '0.00',
 				y: '0.00'
@@ -88,14 +102,17 @@ export default {
 		}
 	},
 	watch: {
-		model: {
+		'model.src': {
+			handler() {
+				if (!this.model.src) {
+					this.model.x = this.getDefaults().x;
+					this.model.y = this.getDefaults().y;
+				}
+			}
+		},
+		'model': {
 			handler(value) {
 				this.$emit('changed-model', value);
-
-				console.log('src', this.model.src);
-				if (!this.model.src) {
-					console.log('null', this.model.src);
-				}
 			},
 			deep: true
 		}
@@ -103,4 +120,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.bordered {
+	border: 1px solid #e0e0e0;
+}
+</style>
